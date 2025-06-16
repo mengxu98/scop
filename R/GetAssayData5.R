40c58101d6cb0ff9a0ab213d1b84ef092b06b6c2
Meng Xu <86040828+mengxu98@users.noreply.github.com>
2025-06-16 20:46:59 +0800

    [func] update `GetAssayData5` to S4 methods

--- R/GetAssayData5.R
+++ R/GetAssayData5.R
@@ -4,6 +4,7 @@
 #'
 #' @md
 #' @inheritParams SeuratObject::GetAssayData
+#' @param join.layers Logical, whether to join layers if the object is an Assay5 object.
 #' @param verbose Logical, whether to print messages.
 #' @param ... Additional arguments passed to [SeuratObject::GetAssayData].
 #'
@@ -25,31 +26,101 @@
 #'   layer = "counts",
 #'   assay = "RNA"
 #' )[1:5, 1:5]
-GetAssayData5 <- function(
+GetAssayData5 <- function(object, ...) {
+  UseMethod(generic = "GetAssayData5", object = object)
+}
+
+#' @rdname GetAssayData5
+#' @method GetAssayData5 Seurat
+#' @export
+GetAssayData5.Seurat <- function(
+    object,
+    layer = "counts",
+    assay = NULL,
+    join.layers = TRUE,
+    verbose = TRUE,
+    ...) {
+  assay <- assay %||% SeuratObject::DefaultAssay(object = object)
+  assay_obj <- Seurat::GetAssay(
+    object = object,
+    assay = assay,
+    ...
+  )
+  data <- GetAssayData5(
+    object = assay_obj,
+    layer = layer,
+    join.layers = join.layers,
+    verbose = verbose,
+    ...
+  )
+  return(data)
+}
+
+#' @rdname GetAssayData5
+#' @method GetAssayData5 Assay5
+#' @export
+GetAssayData5.Assay5 <- function(
+    object,
+    layer = "counts",
+    join.layers = TRUE,
+    verbose = TRUE,
+    ...) {
+  if (verbose && join.layers) {
+    warning_key <- "assay5_join_layers_warning"
+    if (!exists(".scop_env", envir = .GlobalEnv)) {
+      assign(".scop_env", new.env(), envir = .GlobalEnv)
+    }
+    last_warning_time <- if (exists(warning_key, envir = .GlobalEnv$.scop_env)) {
+      get(warning_key, envir = .GlobalEnv$.scop_env)
+    } else {
+      as.numeric(Sys.time()) - 30000
+    }
+
+    current_time <- as.numeric(Sys.time())
+    warning_interval <- getOption("scop.warning.interval", 28800)
+    if (current_time - last_warning_time > warning_interval) {
+      warning(
+        "The input data is a 'Assay5' object. The 'SeuratObject::JoinLayers' function will be used to combine the layers.",
+        immediate. = TRUE
+      )
+      message(
+        "This warning will be shown only once every ",
+        warning_interval / 3600,
+        " hours. To change this interval, set the 'scop.warning.interval' option."
+      )
+      assign(warning_key, current_time, envir = .GlobalEnv$.scop_env)
+    }
+
+    object <- SeuratObject::JoinLayers(object)
+    data <- SeuratObject::GetAssayData(
+      object,
+      layer = layer,
+      ...
+    )
+  } else {
+    data <- SeuratObject::LayerData(
+      object,
+      layer = layer,
+      ...
+    )
+  }
+
+  return(data)
+}
+
+#' @rdname GetAssayData5
+#' @method GetAssayData5 Assay
+#' @export
+GetAssayData5.Assay <- function(
     object,
     layer = "counts",
     assay = NULL,
     verbose = TRUE,
     ...) {
-  assay <- assay %||% SeuratObject::DefaultAssay(object)
-  assay_obj <- Seurat::GetAssay(object, assay = assay)
-
-  if (inherits(assay_obj, "Assay5")) {
-    if (verbose) {
-      warning(
-        "The input data is a 'Assay5' object. The 'SeuratObject::JoinLayers' function will be used to combine the layers.",
-        immediate. = TRUE
-      )
-      object <- SeuratObject::JoinLayers(object)
-    }
-  }
-
   data <- SeuratObject::GetAssayData(
     object,
     layer = layer,
-    assay = assay,
     ...
   )
-
   return(data)
 }
