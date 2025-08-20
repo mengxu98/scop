c871ec142b3e0cddffec6bbe63378edbc57a56a7
Meng xu <86040828+mengxu98@users.noreply.github.com>
2025-08-20 13:54:53 +0800

    [func] remove `download` function

--- R/utils.R
+++ R/utils.R
@@ -1,61 +1,3 @@
-#' @title Download file from the Internet
-#'
-#' @md
-#' @inheritParams utils::download.file
-#' @param methods Methods to be used for downloading files. 
-#' The default is to try different download methods in turn until the download is successfully completed.
-#' @param max_tries Number of tries for each download method.
-#' @param ... Other arguments passed to [utils::download.file].
-#'
-#' @export
-download <- function(
-    url,
-    destfile,
-    methods = c(
-      "auto", "wget", "libcurl", "curl", "wininet", "internal"
-    ),
-    quiet = FALSE,
-    ...,
-    max_tries = 2) {
-  if (missing(url) || missing(destfile)) {
-    stop("'url' and 'destfile' must be both provided.")
-  }
-  ntry <- 0
-  status <- NULL
-  while (is.null(status)) {
-    for (method in methods) {
-      status <- tryCatch(
-        expr = {
-          suppressWarnings(
-            utils::download.file(
-              url = url,
-              destfile = destfile,
-              method = method,
-              quiet = quiet,
-              ...
-            )
-          )
-          status <- 1
-        }, error = function(error) {
-          log_message(error)
-          log_message("Cannot download from the url: ", url)
-          log_message("Failed to download using \"", method, "\". Retry...\n")
-          Sys.sleep(1)
-          return(NULL)
-        }
-      )
-      if (!is.null(status)) {
-        break
-      }
-    }
-    ntry <- ntry + 1
-    if (is.null(status) && ntry >= max_tries) {
-      log_message("Download failed.", message_type = "error")
-    }
-  }
-  return(invisible(NULL))
-}
-
 kegg_get <- function(url) {
   temp <- tempfile()
   on.exit(unlink(temp))
@@ -66,15 +8,15 @@ kegg_get <- function(url) {
       strsplit(readLines(temp), split = "\t")
     )
   )
-  return(content)
+  content
 }
 
 col2hex <- function(cname) {
-  colMat <- grDevices::col2rgb(cname)
+  col_mat <- grDevices::col2rgb(cname)
   grDevices::rgb(
-    red = colMat[1, ] / 255,
-    green = colMat[2, ] / 255,
-    blue = colMat[3, ] / 255
+    red = col_mat[1, ] / 255,
+    green = col_mat[2, ] / 255,
+    blue = col_mat[3, ] / 255
   )
 }
 
@@ -87,5 +29,5 @@ select_cells <- function(obj, celltypes, group.by) {
       rownames(metadata[metadata[[group.by]] == celltype, ])
     )
   }
-  return(cells_c)
+  cells_c
 }
