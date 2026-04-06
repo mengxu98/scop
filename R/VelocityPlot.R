e30fb7b28f13a9ef2c4642b1b414f6600964aad0
Meng xu <86040828+mengxu98@users.noreply.github.com>
2026-04-06 16:21:05 +0800

    docs: update documentation

--- R/VelocityPlot.R
+++ R/VelocityPlot.R
@@ -74,37 +74,37 @@
 #' )
 #' VelocityPlot(
 #'   pancreas_sub,
-#'   reduction = "UMAP"
+#'   reduction = "umap"
 #' )
 #'
 #' VelocityPlot(
 #'   pancreas_sub,
-#'   reduction = "UMAP",
+#'   reduction = "umap",
 #'   group.by = "SubCellType"
 #' )
 #'
 #' VelocityPlot(
 #'   pancreas_sub,
-#'   reduction = "UMAP",
+#'   reduction = "umap",
 #'   plot_type = "grid"
 #' )
 #'
 #' VelocityPlot(
 #'   pancreas_sub,
-#'   reduction = "UMAP",
+#'   reduction = "umap",
 #'   plot_type = "stream"
 #' )
 #'
 #' VelocityPlot(
 #'   pancreas_sub,
-#'   reduction = "UMAP",
+#'   reduction = "umap",
 #'   plot_type = "stream",
 #'   streamline_color = "black"
 #' )
 #'
 #' VelocityPlot(
 #'   pancreas_sub,
-#'   reduction = "UMAP",
+#'   reduction = "umap",
 #'   plot_type = "stream",
 #'   streamline_color = "black",
 #'   arrow_color = "red"
